1fedc21fe2da8f72229dc0fa77979f8a58d93f8d
Diogo Trindade <diogotr7@gmail.com>
2023-05-24 18:03:18 +0100

    Added layout exporting
    fixed save file picker

--- src/Artemis.UI/Screens/Device/Tabs/DevicePropertiesTabViewModel.cs
+++ src/Artemis.UI/Screens/Device/Tabs/DevicePropertiesTabViewModel.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
+using System.Linq;
 using System.Reactive.Disposables;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 using Artemis.Core;
 using Artemis.Core.Services;
 using Artemis.UI.Shared;
 using Artemis.UI.Shared.Services;
 using Artemis.UI.Shared.Services.Builders;
 using ReactiveUI;
+using RGB.NET.Layout;
 using SkiaSharp;
 
 namespace Artemis.UI.Screens.Device;
@@ -275,5 +279,66 @@ public class DevicePropertiesTabViewModel : ActivatableViewModelBase
     {
         if (e.PropertyName == nameof(Device.CustomLayoutPath) || e.PropertyName == nameof(Device.DisableDefaultLayout))
             Task.Run(() => _rgbService.ApplyBestDeviceLayout(Device));
+    }
+    
+    public async Task ExportLayout()
+    {
+        string fileName = Device.DeviceProvider.GetDeviceLayoutName(Device);
+        string layoutDir = Constants.LayoutsFolder;
+        string filePath = Path.Combine(
+            layoutDir,
+            Device.RgbDevice.DeviceInfo.Manufacturer,
+            Device.DeviceType.ToString(),
+            fileName
+        );
+        if (!Directory.Exists(filePath))
+            Directory.CreateDirectory(filePath);
+        
+        string? result = await _windowService.CreateSaveFileDialog()
+            .HavingFilter(f => f.WithExtension("xml").WithName("Artemis layout"))
+            .WithDirectory(filePath)
+            .WithInitialFileName(fileName)
+            .ShowAsync();
+        
+        if (result == null)
+            return;
+        
+        ArtemisLayout? layout = Device.Layout;
+        if (layout?.IsValid == true)
+        {
+            string path = layout.FilePath;
+            File.Copy(path, result, true);
+        }
+        else
+        {
+            List<LedLayout> ledLayouts = Device.Leds.Select(x =>
+            {
+                return new LedLayout()
+                {
+                    Id = x.RgbLed.Id.ToString(),
+                    DescriptiveX = x.Rectangle.Left.ToString(),
+                    DescriptiveY = x.Rectangle.Top.ToString(),
+                    DescriptiveWidth = $"{x.Rectangle.Width}mm",
+                    DescriptiveHeight = $"{x.Rectangle.Height}mm",
+                };
+            }).ToList();
+
+            DeviceLayout emptyLayout = new()
+            {
+                Author = "Artemis",
+                Type = Device.DeviceType,
+                Vendor = Device.RgbDevice.DeviceInfo.Manufacturer,
+                Model = Device.RgbDevice.DeviceInfo.Model,
+                Width = Device.Rectangle.Width,
+                Height = Device.Rectangle.Height,
+                InternalLeds = ledLayouts,
+            };
+
+            XmlSerializer serializer = new(typeof(DeviceLayout));
+            await using StreamWriter writer = new(result);
+            serializer.Serialize(writer, emptyLayout);
+        }
+
+        _notificationService.CreateNotification().WithMessage("Layout exported").WithTimeout(TimeSpan.FromSeconds(5)).Show();
     }
 }
